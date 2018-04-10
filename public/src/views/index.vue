33d5eb8a3ab1bc65eb1f0f27c62a2f849306e398
勺夕 <shaoxi2093@foxmail.com>
2018-04-10 21:44:02 +0800

    初始化页面组件

---
+++ public/src/views/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class=""></div>
+</template>
+
+<script>
+    export default {
+        name: 'vuecomp',
+        data: () => {
+            return {}
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
